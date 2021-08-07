1751f7d9d19ea2a1f6ad06ec08e8a52bb0a9e725
kimchijoa <amsmdmfm159@naver.com>
2021-08-08 00:15:13 +0900

    asdf

--- app/api/api/v1/logins.rb
+++ app/api/api/v1/logins.rb
@@ -1,6 +1,8 @@
 module V1
     class Logins < Grape::API
 
+      use ActionDispatch::Session::CookieStore, key: '_login_session'
+
       resource :logins do
 
         desc "Return all User"
@@ -63,6 +65,11 @@ module V1
               @token = JWT.encode({ user_id: @login_result.id, exp: 30.days.from_now.to_i }, ENV["SECRET_KEY_BASE"])	
               @tree = { :"JWT token" => @token, :userInfo => {id: @login_result.id, email: @login_result.email} }
               puts "JWT토큰이 생성되었습니다.#{@token}"
+              request.headers.each |key, value|
+                key
+              end
+              request['authorization'] = "Token token=#{@token}"
+              return @token
             else
               status 400
               puts "비밀번호가 일치하지 않습니다."
