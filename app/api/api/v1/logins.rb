3f75b757ffd532c3775c965b5aa40adde1a0d8b3
kimchijoa <amsmdmfm159@naver.com>
2021-08-12 21:00:50 +0900

    asdf

--- app/api/api/v1/logins.rb
+++ app/api/api/v1/logins.rb
@@ -65,7 +65,7 @@ module V1
               puts "JWT토큰이 생성되었습니다.#{@token}"
               # response header의 Authorization 값을 토큰값으로 변경한다.
               header['Authorization'] = "#{@token}"
-              #return @token
+              return @token
             else
               status 400
               puts "비밀번호가 일치하지 않습니다."
@@ -81,36 +81,29 @@ module V1
         end
 
         get "access_auth" do
-          @re_token = params[:key]
+          re_token = params[:key]
+          puts "전달받은 토큰 : #{re_token}"
           #토큰 검증과정
-          def http_token
-            http_token ||= if @re_token.present?
-              @re_token.split(' ').last
-            end
+          if re_token.present?
+            http_token =  re_token.split(' ').last
           end
 
           ## 토큰 해석 : 토큰 해석은 lib/json_web_token.rb 내의 decode 메소드에서 진행됩니다.
-          def auth_token
-              auth_token ||= JsonWebToken.decode(http_token)
-          end
+          auth_token ||= JsonWebToken.decode(http_token)
 
           ## 토큰 해석 후, Decode 내용 중 User id 정보 확인
-          def user_id_in_token?
-            http_token && auth_token && auth_token[:user_id].to_i
-          end
+          user_id_in_token =  http_token && auth_token && auth_token[:user_id].to_i
 
-
-          unless user_id_in_token?
-            render json: { errors: ['Not Authenticated'] }, status: :unauthorized
-            return
+          unless user_id_in_token
+            puts "인증실패"
           end
 
           ## Token 안에 있는 user_id 값을 받아와서 User 모델의 유저 정보 탐색
           @current_user = User.find(auth_token[:user_id])
           rescue JWT::VerificationError, JWT::DecodeError
-          render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+            puts "유저정보 탐색 실패"
 
-            puts "You Want this rs?? : #{params[:key]}"
+          puts "You Want this rs?? : #{params[:key]}"
         end
           
 
